f9190093692b47ae47a5cd5c574431c09464f7fe
hwaphon <969489625@qq.com>
2018-05-07 19:39:13 +0800

    初步tabbar

---
+++ src/pages/community/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Community</div>
+</template>
+
+<script>
+
+export default {
+};
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/videos/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Videos</div>
+</template>
+
+<script>
+
+export default {
+};
+
+</script>
+
+<style scoped>
+</style>
