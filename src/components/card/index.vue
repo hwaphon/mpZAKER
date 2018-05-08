dd6fb544e6e036ced09e5d344ee88348fbec98dd
hwaphon <969489625@qq.com>
2018-05-08 17:58:30 +0800

    新增Card组件

---
+++ src/components/card/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Card">
+    <div class="card-img-container" :class="bgClass">
+      <img :src="icon" alt="icon" class="card-img">
+    </div>
+    <span class="card-title">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    bgClass() {
+      const length = 4;
+      const index = Math.floor(Math.random() * length);
+      return `card-img-bg-${index}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -3,12 +3,22 @@
     <nav class="Info-nav" :style="{ marginTop: getSBH + 'px', height: 2 * getSBH + 'px' }">
       <Topbar @click="changeTab"></Topbar>
     </nav>
+    <section class="Info-top">
+      <div class="top-container">
+        <p class="top-title">热门栏目</p>
+        <a href="#" class="top-href">查看全部</a>
+      </div>
+      <div class="top-card">
+        <Card v-for="(item, index) in topItems" :key="index" :icon="item.icon" :text="item.text"></Card>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
 import store from '@/store';
-import Topbar from '@/components/topbar';
+import Topbar from '@/components/topbar/index';
+import Card from '@/components/card/index';
 
 export default {
   computed: {
@@ -18,11 +28,21 @@ export default {
   },
   components: {
     Topbar,
+    Card,
   },
   methods: {
     changeTab(index) {
       console.log(index);
     },
+  },
+  data() {
+    return {
+      topItems: [
+        { icon: '/static/images/moring.png', text: '早报' },
+        { icon: '/static/images/ball.png', text: '今日体坛' },
+        { icon: '/static/images/message.png', text: '十说今日' },
+      ],
+    };
   },
 };
 
